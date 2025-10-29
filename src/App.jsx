f28be47ecafda740c63fb12d47425f4051321b91
lucas ruiz <ruizlukario@gmail.com>
2025-10-28 21:00:16 -0300

    Subo los routers en el develop

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
 function App() {
 
   return (
-    <>
-    </>
+   <BrowserRouter>
+      <Routes>
+        {/* Rutas de la aplicación */}
+        <Route
+          path="/"
+          element={
+            
+            <div>
+              <h1 className='text-red-600'>hola</h1>
+            </div>
+          }
+        />
+
+        {/* Página de error 404 */}
+        <Route path="*" element={<Error404 />} />
+      </Routes>
+    </BrowserRouter>
+
   )
 }
 
