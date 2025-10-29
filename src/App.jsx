a0bf066b7264b744052185345f9b9a3da8c38c82
lucas ruiz <ruizlukario@gmail.com>
2025-10-28 21:17:19 -0300

    Agregue el diseños en el react y css e import en react

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,7 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Error404 from "./page/Error404.jsx";
+
+
 function App() {
 
   return (
--- src/page/Error404.jsx
+++ src/page/Error404.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import { Link } from "react-router-dom";
+import "./error404.css"
+
 
 function Error404() {
   return (
-    <div>
-      
+    
+    <div className="error-container">
+      <h1 className="error-404">404</h1>
+      <p className="error-text">Nivel no encontrado 🕹️</p>
+      <Link to="/" className="home-button">
+        Volver al inicio
+      </Link>
     </div>
+    
   )
 }
 
