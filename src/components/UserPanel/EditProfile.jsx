44b923b5cd2ce1edeb5009590d95c8ffac84f83d
ana <laboralanaluz.a.b@gmail.com>
2026-02-10 16:33:32 -0300

    EditProfile.jsx imports

---
+++ src/components/UserPanel/EditProfile.jsx
@@ -0,0 +1,3 @@
+import React, { useState } from 'react';
+import { FaUser, FaEnvelope, FaCamera, FaSave } from 'react-icons/fa';
+import { updateUserProfile, uploadProfileImage } from '../../helpers/userQueries';
